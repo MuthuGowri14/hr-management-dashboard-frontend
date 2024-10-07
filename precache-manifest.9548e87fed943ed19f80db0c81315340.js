b7949dff29469c53deb71212691314e6532881d3
MuthuGowri14 <kmuthugowri@gmail.com>
2024-10-07 16:02:40 +0530

    Updates

---
+++ precache-manifest.9548e87fed943ed19f80db0c81315340.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d6963834af08186a51d55cb1f17a92",
+    "url": "/hr-management-dashboard-frontend/index.html"
+  },
+  {
+    "revision": "b7d85707fc1937ba82eb",
+    "url": "/hr-management-dashboard-frontend/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "847ccfc66e9e085085a9",
+    "url": "/hr-management-dashboard-frontend/static/css/main.c92059f2.chunk.css"
+  },
+  {
+    "revision": "b7d85707fc1937ba82eb",
+    "url": "/hr-management-dashboard-frontend/static/js/2.7332dfd0.chunk.js"
+  },
+  {
+    "revision": "a70cfba45799f8a4deb5",
+    "url": "/hr-management-dashboard-frontend/static/js/3.cf2aeaea.chunk.js"
+  },
+  {
+    "revision": "847ccfc66e9e085085a9",
+    "url": "/hr-management-dashboard-frontend/static/js/main.7112bae2.chunk.js"
+  },
+  {
+    "revision": "0d34b695410c1d9efa0e",
+    "url": "/hr-management-dashboard-frontend/static/js/runtime~main.b2c36e89.js"
+  }
+]);
